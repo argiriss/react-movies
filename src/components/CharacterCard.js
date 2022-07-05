795a94e755178fbea8dfc3e665f7b39689e23995
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-07-05 12:42:21 +0300

    Set characters content

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -6,7 +6,14 @@ const CharacterCard = ({ character }) => {
   return (
     <div className="card">
       <div className="card-image" style={{ backgroundImage: `url('${character.image}')` }} />
-      <div className="card-content">content</div>
+      <div className="card-content">
+        <h1>{character.name}</h1>
+        <span>
+          {character.status} - {character.species}
+        </span>
+        <div>Last known location: {character.location.name}</div>
+        <div>First seen in: {character.origin.name}</div>
+      </div>
     </div>
   );
 };
