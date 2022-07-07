6a4b3636683d7d450c279da96a74b6e0154f3a56
Argiris Anastasakis <anastasakis.an@gmail.com>
2022-07-07 11:20:29 +0300

    Add packages

--- src/store.js
+++ src/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+// import rootEpics from 'models/epics/rootEpics';
+import { createEpicMiddleware } from 'redux-observable';
 
 import charactersReducer from './models/reducers/characters';
 
+const epicMiddleWare = createEpicMiddleware();
+
 const store = configureStore({
   reducer: { charactersReducer },
+  middleware: [epicMiddleWare],
   devTools: process.env.NODE_ENV !== 'production',
 });
+
+// epicMiddleWare.run(rootEpics);
 
 export default store;
