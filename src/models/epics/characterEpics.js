d4dbf257ef068be9af743701b0698b39b7e5190c
Vaios Anastasiadis <vaios.anastasiadis@gfk.com>
2022-07-08 11:11:29 +0300

    Return observable instead of promise with fetch in order to be manageable from mergeMap

--- src/models/epics/characterEpics.js
+++ src/models/epics/characterEpics.js
@@ -1,32 +1,24 @@
 import { fetchCharactersAction, setCharactersList, setError } from 'models/actions/characterActions';
 import { ofType } from 'redux-observable';
-// import { fromPromise } from 'rxjs';
-import { concatMap } from 'rxjs/operators';
+import { from } from 'rxjs';
+import { concatMap, mergeMap } from 'rxjs/operators';
 
 const fetchCharactersEpic = action$ =>
   action$.pipe(
     ofType(fetchCharactersAction.type),
-    concatMap(async ({ payload: { name, status, gender, page } }) => {
+    mergeMap(({ payload: { name, status, gender, page } }) => {
       const url = `${process.env.REACT_APP_RICKANDMORTY}/character?page=${page}&name=${name}&status=${status}&gender=${gender}`;
-      const characters = await fetch(url);
-      const returnedCharacters = await characters.json();
+      const returnedCharacters = from(fetch(url).then(data => data.json()));
 
-      // LETS SAY THAT BUSINESS REQUIRED
-      // AN EXTRA FIELD ON THE RESULTS e.g. isVaios: true
-      // THIS MANIPULATION GOES ALWAYS TO AN EPIC AND NOT DIRECTLY TO THE REDUCER
-      const newReturnedCharacters = {
-        ...returnedCharacters,
-        results: returnedCharacters?.results?.map(result => ({ ...result, isVaios: true })),
-      };
+      return returnedCharacters.pipe(
+        concatMap(payload => {
+          if (payload?.error === '' || !payload?.error) {
+            return [setCharactersList(payload), setError('')];
+          }
 
-      // equals to dispatch(setCharactersList(returnedCharacters)) from component
-      if (returnedCharacters?.error === '' || !returnedCharacters?.error) {
-        // debugger;
-
-        return setCharactersList(newReturnedCharacters);
-      }
-
-      return setError(returnedCharacters?.error);
+          return [setError(payload?.error)];
+        })
+      );
     })
   );
 
